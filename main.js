1e9a8c9fd623f5245b60c7bf0f73f33a151d17c5
Kamil <kamilrayman1@gmail.com>
2022-10-21 17:44:11 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+fetch("periodic_table_data.json")
+  .then(response => response.json())
+  .then(json => generate_periodic_table(json));
+function generate_periodic_table(json_data){
+    for(element in json_data){
+        let periodic_table = document.querySelector("#periodic_table");
+        let chem_data = json_data[element];
+        let new_chem_el = document.createElement("div");
+        new_chem_el.classList.add("chemical_button");
+        if(chem_data.atomic_number == 0){
+            new_chem_el.classList.add("empty");
+        }
+        if(chem_data.symbol !== undefined){
+            new_chem_el.innerHTML=`<span><sub>${chem_data.atomic_number}</sub>${chem_data.symbol}</span><span>${chem_data.name}</span>`; 
+        }
+        periodic_table.appendChild(new_chem_el);
+    }
+}
